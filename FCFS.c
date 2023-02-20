99202e1499f3dc0f901083af1e2e3fdf12a7995c
Brody Abner <brodytabner@protonmail.com>
2023-02-20 17:03:41 -0600

    Create FCFS.c

---
+++ FCFS.c
@@ -0,0 +1,10 @@
+/*
+First Come First Serve scheduling algorithm
+schedules processes based on arrival time, with processes that arrive first being scheduled first.
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+}
